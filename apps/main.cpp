5418d002aa56352651433811eb96e58a32a2346d
Alice Rixte <alice.rixte@gmail.com>
2023-07-13 22:22:41 +0200

    First commit.

---
+++ apps/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "graspgl.h"
+
+int main(int, char**) {
+    std::string helloJim = generateHelloString("Jim");
+    std::cout << helloJim << std::endl;
+
+    return 0;
+}
